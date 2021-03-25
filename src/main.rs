1934bef51ab7bd20c96a8f36527026c2bba01f11
Frederik Baetens <baetens.fr@gmail.com>
2021-03-25 23:56:20 +0100

    move everything into main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod noderef;
-use noderef::NodeRef;
+use serde::{Deserialize, Serialize//};
+use std::net::SocketAddr;
 
 fn main() {
     let addr = "0.0.0.0:8000".parse().unwrap();
@@ -8,3 +8,28 @@ fn main() {
     noderef.upsert("hi".to_owned(), "hey".to_owned());
     noderef.upsert_prefix_ref("hi", addr);
 }
+
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+pub struct NodeRef {
+    pub address: SocketAddr,
+}
+
+impl NodeRef {
+    pub fn new(address: SocketAddr) -> NodeRef {
+        NodeRef { address }
+    }
+
+    pub  fn get(&self, key: &str) -> Option<String> {
+        dbg!(key);
+        None
+    }
+
+    pub  fn upsert(&self, key: String, value: String) {
+        dbg!(key, value);
+    }
+
+    pub  fn upsert_prefix_ref(&self, prefix: &str, addr: SocketAddr) {
+        dbg!(prefix, addr);
+    }
+}
+
